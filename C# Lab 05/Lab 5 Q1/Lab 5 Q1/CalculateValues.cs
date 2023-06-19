2d3169c41349bcb60ee78259678358798c54d481
Thushan W A R T <125721398+RusithTharindu@users.noreply.github.com>
2023-06-19 15:19:32 +0530

    Add files via upload

---
+++ C# Lab 05/Lab 5 Q1/Lab 5 Q1/CalculateValues.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_5_Q1
+{
+    internal class CalculateValues
+    {
+        public void Addition(int no1, int no2)
+        {
+            int ans;
+            ans= no1 + no2;
+            Console.WriteLine("Your answer is: " + ans);
+        }
+
+        public void Subtraction(int no1, int no2)
+        {
+            int ans;
+            ans= no1 - no2;
+            Console.WriteLine("Your answer is: " + ans);
+
+        }
+
+        public void Multiplication(int no1, int no2)
+        {
+            int ans;
+            ans= no1 * no2;
+            Console.WriteLine("Your answer is: " + ans);
+        }
+        public void Division(int no1, int no2)
+        {
+            int ans;
+            ans= no1 / no2;
+            Console.WriteLine("Your answer is: " + ans);
+        }
+
+
+    }
+}
