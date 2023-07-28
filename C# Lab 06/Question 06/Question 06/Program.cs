e3674bbd56a6ccbb012e230712923f48997c69da
Thushan W A R T <125721398+RusithTharindu@users.noreply.github.com>
2023-07-28 22:39:51 +0530

    Add files via upload

---
+++ C# Lab 06/Question 06/Question 06/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Question_06
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            newclass obj = new newclass();
+            int size = 0 ;
+
+            Console.Write("Enter size value for array: ");
+            size = int.Parse(Console.ReadLine());
+
+            obj.Arraycal(size);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ C# Lab 06/Question 06/Question 06/newclass.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Question_06
+{
+    internal class newclass
+    {
+        public void Arraycal(int size)
+        {
+            int i;
+            
+            int[] array = new int[size];
+
+            for(i=0; i<size; i++)
+            {
+                Console.Write("Enter a value for "+i+": ");
+                array[i] = int.Parse(Console.ReadLine());
+
+            }
+
+            for(i = 0; i < size; i++)
+            {
+                if(i %2 != 0)
+                {
+                    int x = 0;
+                    array[i] = x;
+                }
+
+                Console.Write(" " + array[i]);
+            }
+
+
+
+        }
+
+    }
+}
