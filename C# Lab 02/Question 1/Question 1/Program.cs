caa10845cbef6de280303ade5a6bc090afea39b5
Thushan W A R T <125721398+RusithTharindu@users.noreply.github.com>
2023-07-28 21:25:41 +0530

    Add files via upload

---
+++ C# Lab 02/Question 1/Question 1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Question_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2, num3;
+
+            Console.Write("Enter the first Number: ");
+            num1 = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter the second number: ");
+            num2 = int.Parse(Console.ReadLine());
+
+            num3 = num1 + num2;
+
+            Console.WriteLine("The answer is : " + num3);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ C# Lab 02/Question 4/Question 4/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Question_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            Console.WriteLine("ODD and EVEN checker\n");
+            Console.Write("Enter the number: ");
+            num = int.Parse(Console.ReadLine());
+
+            if(num % 2  == 0) 
+            {
+                Console.WriteLine("This is an EVEN number");
+            }
+            else
+            {
+                Console.WriteLine("This is an ODD number");
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
