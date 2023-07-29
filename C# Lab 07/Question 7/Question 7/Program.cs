6c7e1813a198b91111737dcc15b985fd5ce2b6b8
Thushan W A R T <125721398+RusithTharindu@users.noreply.github.com>
2023-07-29 10:05:12 +0530

    Add files via upload

---
+++ C# Lab 07/Question 7/Question 7/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Question_7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the size of the arrays: ");
+            int size = int.Parse(Console.ReadLine());
+
+            double[] array1 = new double[size];
+            double[] array2 = new double[size];
+
+            
+            Console.WriteLine("Enter values for array1:");
+            for (int i = 0; i < size; i++)
+            {
+                Console.Write($"Element {i + 1}: ");
+                array1[i] = double.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("Enter values for array2:");
+            for (int i = 0; i < size; i++)
+            {
+                Console.Write($"Element {i + 1}: ");
+                array2[i] = double.Parse(Console.ReadLine());
+            }
+
+            //Scalar Sum
+            double scalarSum = 0;
+            for (int i = 0; i < size; i++)
+            {
+                scalarSum += array1[i] + array2[i];
+            }
+            Console.WriteLine("Scalar Sum: " + scalarSum);
+
+            //Vector Sum
+            double[] vectorSum = new double[size];
+            for (int i = 0; i < size; i++)
+            {
+                vectorSum[i] = array1[i] + array2[i];
+            }
+            Console.Write("Vector Sum: ");
+            PrintArray(vectorSum);
+
+            //Vector Product
+            double[] vectorProduct = new double[size];
+            for (int i = 0; i < size; i++)
+            {
+                vectorProduct[i] = array1[i] * array2[i];
+            }
+            Console.Write("Vector Product: ");
+            PrintArray(vectorProduct);
+
+            //Scalar Product and sum
+            double[] scalarProduct = new double[size];
+            double scalarProductSum = 0;
+            for (int i = 0; i < size; i++)
+            {
+                scalarProduct[i] = array1[i] * array2[i];
+                scalarProductSum += scalarProduct[i];
+            }
+            Console.Write("Scalar Product: ");
+            PrintArray(scalarProduct);
+            Console.WriteLine("Sum of Scalar Product: " + scalarProductSum);
+            Console.ReadKey();
+        }
+
+        static void PrintArray(double[] arr)
+        {
+            Console.Write("[");
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write(arr[i]);
+                if (i < arr.Length - 1)
+                {
+                    Console.Write(", ");
+                }
+            }
+            Console.WriteLine("]");
+        }
+    }
+}
---
+++ C# Lab 07/Question 8/Question 8/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Question_8
+{
+    class Animal
+    {
+        public void Animals() 
+        {
+            Console.Write("I am Animal ");
+        }
+    }
+
+    class Dog
+    {
+        public void Dogs()
+        {
+            Console.Write("I have four legs.");
+        }
+
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal obj1 = new Animal();
+            Dog obj2 = new Dog();
+
+            obj1.Animals();
+            obj2.Dogs();
+
+            Console.ReadKey(); 
+
+        }
+    }
+}
